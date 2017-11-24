394e0699a0cd6a9678939bc3f610d692671f3ab3
Daniel Peters <daniel.peters.ch@gmail.com>
2017-11-24 12:41:54 +0100

    Add materialize styles. Fix Image upload

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.render('index', {title: 'ChatMate'})
 })
 
