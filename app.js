b639e2a2e93b2ffb4cab9f445ce9eabdf5aaa3b6
Daniel Peters <daniel.peters.ch@gmail.com>
2018-04-11 21:39:48 +0200

    Update server and dependencies.

--- app.js
+++ app.js
@@ -1,56 +1,48 @@
-'use strict'
-
+const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
-// const favicon = require('serve-favicon')
-const logger = require('morgan')
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const routes = require('./routes/index')
-const users = require('./routes/users')
-const multimedia = require('./routes/multimedia')
+const logger = require('morgan')
+const sassMiddleware = require('node-sass-middleware')
+
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const multimediaRouter = require('./routes/multimedia')
+
 const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(require('less-middleware')(path.join(__dirname, 'public')))
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes)
-app.use('/users', users)
-app.use('/multimedia', multimedia)
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/multimedia', multimediaRouter)
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+app.use((req, res, next) => next(createError(404)))
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500)
-    res.render('error', {message: err.message, error: err})
-  })
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
   res.status(err.status || 500)
-  res.render('error', {message: err.message, error: {}})
+  res.render('error')
 })
 
 module.exports = app
